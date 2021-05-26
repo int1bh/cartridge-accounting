1e32f70dbf5d93e89b579110b7e6f7fbfa7d301d
Alexandr <48558946+int1bh@users.noreply.github.com>
2021-05-26 17:01:50 +0300

    Add files via upload

--- models/Cartridge.js
+++ models/Cartridge.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose')
 const schema = new Schema({
         modelName: {type: String, required: true},                       //наименование картриджа
         registered: {type: Date, default: Date.now()},                   //дата регистрации картриджа
-        barcode: {type: String, required: true},                         //штрихкод
+        barcode: {type: String, required: true, unique: true},           //штрихкод
         barcodeImage: {type: String, default: null},                     //путь до изображения штрихкода
         issued: {type: Boolean, default: false},                         //выдан или нет. Если false, то картридж на складе
         issuedHistory: [{subdivision: {type: String, default: null},     //история выдачи
