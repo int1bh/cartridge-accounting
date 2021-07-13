9e1a027d7dc418c72c8af43bfd7b49fa86756361
Alexandr Dryagin <alexandrdryagin@yandex.ru>
2021-07-13 14:18:43 +0300

    настроены диаграммы

--- client/src/component/ChartsComponent.jsx
+++ client/src/component/ChartsComponent.jsx
@@ -1,49 +1,60 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
-import { Col } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 
 function ChartsComponent() {
-    const options = {
-        legend: {position: 'left',
-                  display: true              },
+const rand = () => Math.floor(Math.random() * 255)
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+        display: true,
+        labels: {
+          color: "black",
+          font: { size: 12 },
+        },
+      },
     }
+  };
 
-    const data = {
-        labels: ['HP 280', 'HP 237', 'HP 12', 'HP 255', 'HP 226', 'HP 264'],
-        datasets: [
-          {
-            label: 'dffs',
-            data: [12, 19, 3, 5, 2],
-            backgroundColor: [
-              'red',
-              'green',
-              'blue',
-              'orange',
-              'black'
-            ],
-            borderColor: [
-                'red',
-                'green',
-                'blue',
-                'orange',
-                'black'
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
-      
-  return(
-      <div className="charts">
-      <Doughnut
-      options={options}
-   data={data}
-   
-   
-   />
-       </div>
+  const data = {
+    labels: ["HP 280", "HP 237", "HP 12", "HP 255", "HP 226", "HP 264"],
+    datasets: [
+      {
+        //label: "dffs",
+        data: [12, 19, 3, 5, 2, 8],
+        backgroundColor: [`rgba(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`],
+        //borderColor: [`rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`, `rgb(${rand()}, ${rand()}, ${rand()})`],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-  ) 
+  return (
+    <Row>
+    <Card className="text-center monitor-card">
+      <Card.Header>На складе</Card.Header>
+      <Card.Body>
+        <Doughnut options={options} data={data} />
+      </Card.Body>
+    </Card>
+    <Card className="text-center monitor-card">
+      <Card.Header>Выданы в отделения</Card.Header>
+      <Card.Body>
+        <Doughnut options={options} data={data} />
+      </Card.Body>
+    </Card>
+    <Card className="text-center monitor-card">
+      <Card.Header>На заправке</Card.Header>
+      <Card.Body>
+        <Doughnut options={options} data={data} />
+      </Card.Body>
+    </Card>
+    </Row>
+    
+  );
 }
 
-export default ChartsComponent
+export default ChartsComponent;
