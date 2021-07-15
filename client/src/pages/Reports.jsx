5ee019458aace1b625b7d4739a568fb6a91a9b93
Alexandr Dryagin <alexandrdryagin@yandex.ru>
2021-07-15 13:58:25 +0300

    -

--- client/src/pages/Reports.jsx
+++ client/src/pages/Reports.jsx
@@ -5,7 +5,9 @@ export const Reports = () => {
     return (
         <div>
             <h1>Отчёты</h1>
-            <p>Страница в разработке</p>
+            <p>Страница в разработке. </p>
+            <p>Ваши предложения по наполнению этого раздела принимаются на GitHub проекта в разделе <a href="https://github.com/int1bh/cartridge-accounting/issues">Issues</a></p>
+            <p>Там же регистрируйте все обнаруженные баги.</p>
         </div>
     )
 }
